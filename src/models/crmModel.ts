8994d8eb09b890d212eec1cf6a0f80a8b03d45f7
Abraham E. Tavarez <abetavarez@icloud.com>
2022-05-11 18:07:41 -0400

    type anotation on schema

--- src/models/crmModel.ts
+++ src/models/crmModel.ts
@@ -1,27 +1,27 @@
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema;
+const Schema: any = {
+  firstName: {
+    type: String,
+    required: 'Enter a first name'
+},
+lastName: {
+    type: String,
+    required: 'Enter a last name'
+},
+email: {
+    type: String
+},
+company: {
+    type: String
+},
+phone: {
+    type: Number
+},
+created_date: {
+   type: Date,
+   default: Date.now 
+}
+}
 
-export const ContactSchema = new Schema({
-    firstName: {
-        type: String,
-        required: 'Enter a first name'
-    },
-    lastName: {
-        type: String,
-        required: 'Enter a last name'
-    },
-    email: {
-        type: String
-    },
-    company: {
-        type: String
-    },
-    phone: {
-        type: Number
-    },
-    created_date: {
-       type: Date,
-       default: Date.now 
-    }
-});
+export const ContactSchema = new mongoose.Schema(Schema)
